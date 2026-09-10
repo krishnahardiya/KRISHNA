70a1b1351d4ed59621885427c432fe23834ba1e0
KRISHNA HARDIYA <krishna9294839372@gmail.com>
2026-09-10 23:29:02 +0530

    Configure static files for production

--- my_club/settings.py
+++ my_club/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'my_club.urls'
@@ -134,3 +135,10 @@ STATICFILER_DIRS =[
 LOGIN_URL ='/login/'
 LOGIN_REDIRECT_URL ='/home/'
 LOGOUT_REDIRECT_URL = '/login/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
